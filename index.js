56ea38dc4814d688e3d530b8466bd6855028d785
Talha Rana <mtalharana093@gmail.com>
2024-12-24 23:54:04 +0500

    Infer and Annotation

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-"use strict";
 // data types
-let x = 10;
+// let x : number = 10;
+// console.log(x);
+// let y : string = "hello Talha";
+// console.log(y);
+// Wrapper Object Type: Number , String , Boolean( Do not use it)
+// let x: Number = new Number(10);
+// console.log(x);
+// Undefined 
+// let x;
+// console.log(x);
+// ====> Undefined Output 
+// Type Inference 
+// let x : number = 10; //annotation
+// console.log(x);
+// x = "Talha";
+// console.log(x);
+var x;
+x = 10;
 console.log(x);
+x = "abc";
+console.log(x);
+// let x: number;  // infer
+// let x = 10;    // infer
+// let x;    
+// x = 10;
+// console.log(x);
+// x = "abc";
+// console.log(x);
+// x = false
+// console.log(x);
--- index.ts
+++ index.ts
@@ -1,4 +1,59 @@
 // data types
 
-let x : number = 10;
+// let x : number = 10;
+// console.log(x);
+
+// let y : string = "hello Talha";
+// console.log(y);
+
+
+
+
+// Wrapper Object Type: Number , String , Boolean( Do not use it)
+
+// let x: Number = new Number(10);
+// console.log(x);
+
+
+
+// Undefined 
+// let x;
+// console.log(x);
+// ====> Undefined Output 
+
+// Type Inference 
+
+// let x : number = 10; //annotation
+// console.log(x);
+// x = "Talha";
+// console.log(x);
+
+// let x;
+// x=10;
+// console.log(x);
+// x= "abc";
+// console.log(x);
+// x= true;
+// console.log(x);
+
+// let x: number;  // infer
+// let x = 10;    // infer
+// let x;    
+// x = 10;
+// console.log(x);
+// x = "abc";
+// console.log(x);
+// x = false
+// console.log(x);
+
+// any types we can assign any type of data string boolean and number:
+let x:any = 0;    
+x = 10;
 console.log(x);
+x = "abc";
+console.log(x);
+x = false
+console.log(x);
+
+// Note: Do not use any type in your code 
+
