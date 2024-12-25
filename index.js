8d60d77fb8d3f4044ef113eb512849ed56c7bf7b
Talha Rana <mtalharana093@gmail.com>
2024-12-25 21:51:09 +0500

    arrays and Types

--- index.js
+++ index.js
@@ -15,11 +15,13 @@
 // console.log(x);
 // x = "Talha";
 // console.log(x);
-var x;
-x = 10;
-console.log(x);
-x = "abc";
-console.log(x);
+// let x;
+// x=10;
+// console.log(x);
+// x= "abc";
+// console.log(x);
+// x= true;
+// console.log(x);
 // let x: number;  // infer
 // let x = 10;    // infer
 // let x;    
@@ -29,3 +31,59 @@ console.log(x);
 // console.log(x);
 // x = false
 // console.log(x);
+// any types we can assign any type of data string boolean and number:
+// let x:any = 0;    
+// x = 10;
+// console.log(x);
+// x = "abc";
+// console.log(x);
+// x = false
+// console.log(x);
+// Note: Do not use any type in your code 
+//=================================================
+// Array  Lecture:
+//Arrays(Single Type value in Array)
+// let array: number[] = [1,2,3,4,5];
+// console.log(array);
+// let array2 : string[] = ["Danyal", "Ali", "Nabeel", "Shahjahan"];
+// console.log(array2);
+//Generic Syntex
+// let array: Array<number> = [1,2,3,4,5];
+// console.log(array);
+// let array: Array<string> = ["Ali","Danyal","Nabeel","Ahmed"];
+// console.log(array);
+// Mix type value in Array:
+//  Array Union Types:
+// let arr3 : (string | number | boolean)[] = ["Talha", 1, true];
+// arr3[3] = "Ali";
+// arr3[4] = 90;
+// console.log(arr3);
+//********************************************************** */
+// Arrays with fixed length and type (Tuple)
+// **********************************************************/
+// let arr4 : [string, number, boolean] = ["Talha", 1, true];
+//  //arr4[3] = "Ali";   // it will generate the error if we add more elements in this array
+// console.log(arr4);
+//ReadOnly Arrays
+// you can only read the array elements but can't write the array
+// let array: readonly number[] = [1,2,3,4,5,6,];
+// // array[6] = 10; // read only access
+// // array.push(90);
+// console.log(array);
+// Type Inference
+// let array = [1,2,3,4,5];
+// array.push(10);
+// console.log(array);
+//Array methods and Type Safety 
+// let array:  number[] = [1,2,3,4,5];
+// // console.log(array);
+// let ans = array.map(x => x <= 3 ? "Value: " +(x*2): x);
+// console.log(ans);
+// multi-dimentional
+var matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    ["Ali", "Rana", 9]
+];
+console.log(matrix);
--- readme.md
+++ readme.md
@@ -5,3 +5,10 @@ npx tsc --init
 Compile TypeScript to JavaScript
 npx tsc
 running commend node index.js
+
+================================================
+Inference matlab kah type ka andaza lagata ha kah string , string ha ye boolean
+annotation ka matlab ha kah hum type btaty hain 
+
+Implicit mtlab kisi ki tarf ishara krna lakin saaf lafzon main nhi
+Explicit mtlab waze krna 
